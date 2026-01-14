c4a3f252220d279d4501b572aeb970f47cfa8fb8
lawalvct@gmail.com <lawalthb@gmail.com>
2026-01-15 00:33:06 +0100

    feat: Integrate TinyMCE editor for blog content in create and edit views

--- resources/views/admin/blogs/create.blade.php
+++ resources/views/admin/blogs/create.blade.php
@@ -48,8 +48,7 @@
 
             <div x-show="type === 'regular'">
                 <label class="block text-sm font-medium text-gray-700 mb-2">Content</label>
-                <textarea name="content" rows="15"
-                          class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent">{{ old('content') }}</textarea>
+                <textarea id="blog-content" name="content">{{ old('content') }}</textarea>
                 @error('content')<span class="text-red-500 text-sm">{{ $message }}</span>@enderror
             </div>
 
@@ -103,3 +102,21 @@
     </form>
 </div>
 @endsection
+
+@push('scripts')
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: '#blog-content',
+        height: 500,
+        menubar: true,
+        plugins: [
+            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+            'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        ],
+        toolbar: 'undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    });
+</script>
+@endpush
